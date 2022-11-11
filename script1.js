cf0d6ce34ca9c69bfb38d5d0086c3078fb8e830b
Felipe Greg <mrfelipesensei@gmail.com>
2022-11-11 17:56:02 -0300

    v 1.2 carrossel

---
+++ script1.js
@@ -0,0 +1,15 @@
+//carrossel//
+var imgs = document.getElementById("img");
+var img = document.querySelectorAll("#img img");
+
+var imagemAtual = 0;
+
+function carrosselAutomatico(){
+    imagemAtual++
+    if(imagemAtual > img.length - 1){
+        imagemAtual = 0;
+    }
+    imgs.style.transform = `translateX(${-imagemAtual * imgs.offsetWidth}px)`
+}
+
+setInterval(carrosselAutomatico, 4000);
